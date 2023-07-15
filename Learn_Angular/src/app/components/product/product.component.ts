ace8740db486f6df0267d34c4ea5b41caedc53db
Murad Mammadzade <mammadzade03@gmail.com>
2023-07-15 10:42:38 +0400

    client-model,Typescript interface,export-import,creative-tim,angular vs angular js

--- Learn_Angular/src/app/components/product/product.component.ts
+++ Learn_Angular/src/app/components/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-product',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent {
-
+   products:Product[]=
+   [{id:1,productName:'Laptop'},
+   {id:2,productName:'Headphone'},
+   {id:3,productName:'Phone'},
+   {id:4,productName:'Playstation'},
+   {id:5,productName:'Xbox'}];
 }
