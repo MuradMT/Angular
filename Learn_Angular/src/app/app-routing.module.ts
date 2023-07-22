07d4b70a10b87d654c905ce4127d922fb4f09e39
Murad Mammadzade <mammadzade03@gmail.com>
2023-07-22 16:40:41 +0400

    Angular Routing(router outlet and app routing module-path and component and path match),event binding-(click),property binding-[class],tsconfig json strictpropertyinitilization:false(if your encounter with initilization error,this code solves your problem)

--- Learn_Angular/src/app/app-routing.module.ts
+++ Learn_Angular/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './components/product/product.component';
+import { TodoComponent } from './components/todo/todo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'products',component:ProductComponent},
+  {path:'todos',component:TodoComponent},
+  {path:'',component:TodoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Learn_Angular/src/app/components/category/category.component.ts
+++ Learn_Angular/src/app/components/category/category.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Category } from 'src/app/models/category';
 
 @Component({
   selector: 'app-category',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./category.component.scss']
 })
 export class CategoryComponent {
-
+   data:Category[]=[
+    {name:'My'},
+    {name:'Your'},
+    {name:'His'},];
+   category:Category;
+   getCategory(item:Category){
+        this.category=item;
+   }
+   getClass(item:Category){
+    if(item==this.category){
+      return "list-group-item active"
+    }else{
+      return "list-group-item"
+    }
+   }
 }
